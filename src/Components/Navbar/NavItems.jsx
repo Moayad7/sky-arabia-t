5ee02dde989df2bae0282f9fc37df2bb6cc82fd8
Moayad7 <mouayad.ahmad7@gmail.com>
2023-10-03 11:44:11 +0300

    Update NavItems.jsx

--- src/Components/Navbar/NavItems.jsx
+++ src/Components/Navbar/NavItems.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 export const NavItems = (props) => {
   return (
-    <div className="mx-4 my-6 md:my-0 whitespace-nowrap block text-center w-full">
+    <div className="mx-7 my-7 md:my-0 whitespace-nowrap block text-center w-full uppercase">
       <li>
-        <Link to={props.url} className={props.color?" w-full text-xl text-slate-800 hover:text-blue-500 bold":" w-full text-xl text-slate-800 md:text-slate-50 lg:text-slate-50 hover:text-blue-500 bold"}>
+        <Link to={props.url} className={props.color?"border-b-4 border-transparent hover:border-blue-800 duration-300 font-bold text-lg w-full text-slate-800 hover:text-blue-500 bold":"border-b-4 border-transparent hover:border-blue-800 duration-300 font-medium text-lg w-full  text-slate-800 md:text-slate-50 lg:text-slate-50 hover:text-blue-500 bold"}>
           {props.name}
         </Link>
       </li>
